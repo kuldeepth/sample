1668b51e45a1adf4d63492664c9cd6e500f677bc
kuldeepth <kuldeepthingbaijam.com>
2018-06-26 15:53:44 +0530

    Initial commit

---
+++ _init_.py
@@ -0,0 +1,13 @@
+# print("hello world by python!")
+# a = "Welcome To Python"
+#print(a)
+import webbrowser
+import time
+url1 = "http://www.youtube.com"
+url2 = "htttp://www.google.com"
+url3 = "hhtp://www.ndtv.com"
+i=1
+while i<4:
+    webbrowser.open(url1)
+
+    i=i+1
---
+++ calc.py
@@ -0,0 +1,17 @@
+#import calculator
+#x = calculator.add(10,20)
+#y = calculator.multiply(10,20)
+#print (x)
+#print (y)
+
+
+'''from calculator import multiply
+multiply(12,13)'''
+
+import os
+list_files = os.listdir("C:\\Users\\lab7-14\\Desktop\\new\\prank")
+print(list_files)
+os.chdir("C:\\Users\\lab7-14\\Desktop\\new\\prank")
+    
+
+
